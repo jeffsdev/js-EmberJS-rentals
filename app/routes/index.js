2cd968614036e6c375cf2dd8edebf8b1fdf01d67
Jeff Seymour <seym1585@gmail.com>
2016-03-29 18:01:54 -0700

    add route for cities

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,7 @@ export default Ember.Route.extend({
 
   model(){
     return Ember.RSVP.hash({
+      cities: this.store.findAll('city'),
       rentals: this.store.findAll('rental'),
       announcements: this.store.findAll('announcement')
     });
