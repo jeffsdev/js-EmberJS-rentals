6542373cfac81bd9a36dae7350a9f53bf9f7cbc9
Jeff Seymour <seym1585@gmail.com>
2016-03-29 17:32:14 -0700

    fix update functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -18,8 +18,8 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
 
-    update(rental, params){
-      Object.keys(params).forEach(function(key){
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
         if(params[key]!==undefined) {
           rental.set(key,params[key]);
         }
